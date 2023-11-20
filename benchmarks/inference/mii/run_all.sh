8f3eb4fb546a4ec9a2f751f1d8dbb63856e6e7e6
Marut Pandya <pandyamarut@gmail.com>
2023-11-20 13:04:08 -0600

    Update run_all.sh

--- benchmarks/inference/mii/run_all.sh
+++ benchmarks/inference/mii/run_all.sh
@@ -14,10 +14,7 @@ for PARAM_SIZE in ${PARAM_SIZES[@]}; do
         python server.py --model_name meta-llama/Llama-2-${PARAM_SIZE}-hf -d ${DEPLOYMENT_NAME} -m ${TP} -b ${RAGGED_BATCH_SIZE} start
 
         DEPLOYMENT_NAME=${DEPLOYMENT_NAME} PROMPT_LENGTH=2600 MAX_NEW_TOKENS=60 bash ./run_benchmark_client.sh
-        DEPLOYMENT_NAME=${DEPLOYMENT_NAME} PROMPT_LENGTH=2600 MAX_NEW_TOKENS=128 bash ./run_benchmark_client.sh
-        DEPLOYMENT_NAME=${DEPLOYMENT_NAME} PROMPT_LENGTH=1200 MAX_NEW_TOKENS=60 bash ./run_benchmark_client.sh
-        DEPLOYMENT_NAME=${DEPLOYMENT_NAME} PROMPT_LENGTH=1200 MAX_NEW_TOKENS=128 bash ./run_benchmark_client.sh
-
+        
         echo "Stopping server"
         python server.py -d ${DEPLOYMENT_NAME} stop
         sleep 120
